055b1c8b9179cb4e6b491bb21be6fa27cabc08d5
Andrew Kaiser <andrew@paradigmstopractices.com>
2019-09-02 17:11:24 -0500

    Project Setup

---
+++ slides/slides.h
@@ -0,0 +1,19 @@
+//
+//  slides.h
+//  slides
+//
+//  Created by Andrew Kaiser on 9/2/19.
+//  Copyright © 2019 Andrew Kaiser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for slides.
+FOUNDATION_EXPORT double slidesVersionNumber;
+
+//! Project version string for slides.
+FOUNDATION_EXPORT const unsigned char slidesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <slides/PublicHeader.h>
+
+
---
+++ slidesapp/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  slidesapp
+//
+//  Created by Andrew Kaiser on 9/2/19.
+//  Copyright © 2019 Andrew Kaiser. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
